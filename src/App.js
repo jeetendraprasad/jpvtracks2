d26a4a3da950b10df433d7b3fcf67320a787f3cc
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2022-02-06 13:06:40 +0530

    formatting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 //import logo from './logo.svg';
-import './App.css';
-import Reports from './components/reports';
-import studentInfo from './AppData/StudentsInfo.json';
-
+import "./App.css";
+import Reports from "./components/reports";
+import studentInfo from "./AppData/StudentsInfo.json";
 
 function App() {
-  return (
-    <Reports studentInfoData={studentInfo}/>
-  );
+  return <Reports studentInfoData={studentInfo} />;
 }
 
 export default App;
--- src/components/reports.js
+++ src/components/reports.js
@@ -3,47 +3,49 @@ import CsvDownload from "react-csv-downloader";
 import { CsvToHtmlTable } from "react-csv-to-table";
 
 class Reports extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    convertJsonToCsv() {
-        let { Parser } = require("json2csv");
+  convertJsonToCsv() {
+    let { Parser } = require("json2csv");
 
-        let json2csvParser = new Parser();
-        let csv = json2csvParser.parse(this.props.studentInfoData);
-        
-        return csv;
-    }
+    let json2csvParser = new Parser();
+    let csv = json2csvParser.parse(this.props.studentInfoData);
 
-    render() {
-        return (
-            <>
-            <div className="col-lg-12 text-center">
-                <h1><u>Data</u></h1>
-            </div>
-    
-            <div className="col-lg-12 text-center">
-                <CsvToHtmlTable
-                data={this.convertJsonToCsv()}
-                csvDelimiter=","
-                tableClassName="table table-striped table-hover"
-                />
-            </div>
-    
-            <div>
-                <CsvDownload className="col-lg-12 text-center"
-                filename="myfile"
-                extension=".csv"
-                seperator=";"
-                datas={this.props.studentInfoData}
-                text="DOWNLOAD"
-                />
-            </div>
-    
-            </>
-        );
-    }
+    return csv;
+  }
+
+  render() {
+    return (
+      <>
+        <div className="col-lg-12 text-center">
+          <h1>
+            <u>Data</u>
+          </h1>
+        </div>
+
+        <div className="col-lg-12 text-center">
+          <CsvToHtmlTable
+            data={this.convertJsonToCsv()}
+            csvDelimiter=","
+            tableClassName="table table-striped table-hover"
+          />
+        </div>
+
+        <div>
+          <CsvDownload
+            className="col-lg-12 text-center"
+            filename="myfile"
+            extension=".csv"
+            seperator=";"
+            datas={this.props.studentInfoData}
+            text="DOWNLOAD"
+          />
+        </div>
+      </>
+    );
+  }
 }
 
 export default Reports;
